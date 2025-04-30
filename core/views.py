1c8dbe0fc27b1911d1de0b72a8dccc5bfc522543
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2025-04-30 15:56:49 +0530

    update

--- core/views.py
+++ core/views.py
@@ -25,7 +25,7 @@ def menu_list(request):
 # def product_list(request):
 #     products = list(Product.objects.values())
 #     return JsonResponse(products, safe=False)
-def products_list(request):
+def product_list_api(request):
     products = Product.objects.all()
     data = []
 
