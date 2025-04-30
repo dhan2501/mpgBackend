2875a92891c2786fa3d6c530397b5c19b14bd61b
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2025-04-30 15:22:04 +0530

    product

--- core/models.py
+++ core/models.py
@@ -123,10 +123,10 @@ class Product(models.Model):
     og_title = models.CharField(max_length=255, blank=True, null=True)
     og_description = models.TextField(blank=True, null=True)
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.name)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
